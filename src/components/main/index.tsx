caaaec495ace16f81bcd1d6166ab3f706a791a6f
Eduardo Lima <dlima78@hotmail.com>
2021-05-13 11:13:21 -0300

    feat: add style to first page

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,6 +1,19 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs e Styled Components!
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a tela com código"
+    />
+  </S.Wrapper>
 )
 export default Main
--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -2,10 +2,20 @@ import { render, screen } from '@testing-library/react'
 
 import Main from '@/components/main'
 
+const makeSut = () => {
+  return render(<Main />)
+}
+
 describe('Main Component', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = makeSut()
     const heading = screen.getByRole('heading', { name: /React Avançado/i })
     expect(heading).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = makeSut()
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
